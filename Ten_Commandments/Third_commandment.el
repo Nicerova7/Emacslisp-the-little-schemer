48ea7dbca4c28ec7dff15cd4cf4b07ba52be1ea3
Nicerova7 <nil_al_7@hotmail.com>
2017-02-17 20:38:34 -0500

    Third commandment

--- Ten_Commandments/Third_commandment.el
+++ Ten_Commandments/Third_commandment.el
@@ -1,3 +1,5 @@
 ;; when building a list, describe the first typical element, and then cons it onto the natural recursion
 
-(else (cons (car (car l)) (firsts (cdr l)) ))
+(t (cons (car (car l)) (firsts (cdr l)) ))
+
+;; t is else-cond in emacs lisp
