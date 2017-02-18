64a933b9532fc2a78e56fce8c12bd7db9742af5f
Nicerova7 <nil_al_7@hotmail.com>
2017-02-17 21:12:20 -0500

    Seventh commandment

---
+++ Ten_Commandments/Seventh_commandment.el
@@ -0,0 +1,12 @@
+;; Recur on the subparts that are of the same nature :
+;; * On the sublists of a list.
+;; * On the subexpressions of an arithmetic expression.
+
+(defun funct
+    (lambda (x)
+      (cond ... ;; I maintain its nature
+	    ... ;; I maintain its nature
+	    ... ;; I maintain its nature
+	    )))
+
+      
