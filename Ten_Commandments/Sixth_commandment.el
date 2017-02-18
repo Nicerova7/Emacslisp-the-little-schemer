e41f2e5cbcbd29065da3ea100e0d232ca2d18ed4
Nicerova7 <nil_al_7@hotmail.com>
2017-02-17 21:06:45 -0500

    Sixth commandment

---
+++ Ten_Commandments/Sixth_commandment.el
@@ -0,0 +1,19 @@
+;; Simplify only after the function is correct
+
+(defun funct (x)
+  ( ... (...)
+	(t (...
+	    ( ....)
+	    (t .... ))))
+
+
+ ;; Simplify ...
+ 
+(defun functsimpl (x)
+  ( ... ( ... )
+	( ... )
+	(t ... )))
+
+;; only after the function is ...
+
+		     
