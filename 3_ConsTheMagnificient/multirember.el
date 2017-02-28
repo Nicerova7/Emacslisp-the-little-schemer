ed886c7516a1694392f5f48f47ccec8f5252794e
Nicerova7 <nil_al_7@hotmail.com>
2017-02-28 17:50:55 -0500

    Multi rember

---
+++ 3_ConsTheMagnificient/multirember.el
@@ -0,0 +1,13 @@
+;; Multirember function
+
+;; Rember in every sublist
+
+(defun multirember
+    (lambda (a lat)
+      (cond ((null lat) '())
+	    ((eq? (car lat) a) (multirember a (cdr lat)))
+	    (t (cons (car lat) (multirember a (cdr lat)))))))
+
+
+(multirember 'a '(a b c a e))  ; ( b c e)
+
