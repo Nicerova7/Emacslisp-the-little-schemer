cb8106af93c89b2ef61415f4dbbbc01162df2c52
Nicerova7 <nil_al_7@hotmail.com>
2017-02-28 17:23:39 -0500

    Subst the first ocurrence

---
+++ 3_ConsTheMagnificient/subst.el
@@ -0,0 +1,18 @@
+;; Subst function
+
+;; Review the last codes and its conditions.
+
+(defun subst
+    (lambda (new old lat)
+      (cond ((null lat) '())
+	    ((eq? (car lat) old) (cons new (cdr lat)))
+	    (t (cons (car lat)
+		     (subst new old (cdr lat)))))))
+
+
+(subst 'a 'b '(a b c))
+; (a a c)
+
+(subst 'topping 'fudge '(ice cream with fudge for dessert)
+; (ice cream with topping for dessert)       
+	     
