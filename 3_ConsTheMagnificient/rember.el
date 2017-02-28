af7cba718777e625c7d8cdc803e9a9927c746c5c
Nicerova7 <nil_al_7@hotmail.com>
2017-02-28 13:12:15 -0500

    Rember function

---
+++ 3_ConsTheMagnificient/rember.el
@@ -0,0 +1,18 @@
+;; Rember function
+
+;; Review 1_Toys , there is some information from 'Cons'.
+
+(defun rember
+    (lambda (a lat)
+       (cond ((null lat) '())
+	     ((eq? a (car lat)) (cdr lat))
+	     (t (cons (car lat)
+		      (rember  a
+                               (cdr lat)))))))
+
+
+(rember 'a '(a b c))    ; (b c)
+(rember 'a '(a a b c))  ; (a b c) , This is important to keep in mind
+
+  
+     
