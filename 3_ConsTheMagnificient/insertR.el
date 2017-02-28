fd3cb16ca25b7089509964ea7fabe8541019e3af
Nicerova7 <nil_al_7@hotmail.com>
2017-02-28 13:31:10 -0500

    Insert Right

---
+++ 3_ConsTheMagnificient/insertR.el
@@ -0,0 +1,17 @@
+;; insertR function
+
+;; We know that lat has at least one element
+
+(defun insertR
+    (lambda (new old lat)
+      (cond ((eq? old (car lat)) (cons old (cons new (cdr lat))))
+	    (t (cons (car lat)
+		     (insertR (new
+			       old
+			       (cdr lat))))))))
+
+
+(insertR 'e 'd '(a b c d f))  ; (a b c d e f)
+(insertR 'b 'a '(a c d e))    ; (a b c d e)
+
+				
