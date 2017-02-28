b454fdf97da08f6142b700b97b0e9277883d8d80
Nicerova7 <nil_al_7@hotmail.com>
2017-02-28 18:06:42 -0500

    MultinsertL emacs lisp

---
+++ 3_ConsTheMagnificient/multiinsertL.el
@@ -0,0 +1,17 @@
+;; MultiinsertL function
+
+(defun multiinsertL
+    (lambda (new old lat)
+      (cond ((null lat) '())
+	    ((eq? (car lat) old) (cons new
+				       (cons old
+					     (multiinsertL new old (cdr lat)))))
+	    (t (cons (car lat)
+		     (multiinsertL new old (cdr lat)))))))
+
+
+(multiinsertL 'e 'a '(a b a c))  ; (e a b e a c)
+
+
+
+
