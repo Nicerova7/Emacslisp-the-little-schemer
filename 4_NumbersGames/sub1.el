9c34b3254d8d8c28cdc69f3ef00d49bd0d7da7ec
Nicerova7 <nil_al_7@hotmail.com>
2017-03-02 12:49:44 -0500

    Sub one

---
+++ 4_NumbersGames/sub1.el
@@ -0,0 +1,16 @@
+;; Sub1 function
+
+;; In this book we consider only nonegative numbers.
+
+(defun sub1
+    (lambda (n)
+      (- n 1)))
+
+
+(sub1 15)  ; 14
+(sub1 8)   ; 7
+(sub1 0)   ; -1 ...but we consider only nonegative numbers in this book.
+
+
+
+
