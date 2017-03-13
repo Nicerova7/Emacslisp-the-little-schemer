8ec46df72dcdeeb488ed256926ce80fef3a65998
Nicerova7 <nil_al_7@hotmail.com>
2017-03-13 18:02:53 -0500

    Insert in right to...

--- 3_ConsTheMagnificient/insertR.el
+++ 3_ConsTheMagnificient/insertR.el
@@ -2,13 +2,12 @@
 
 ;; We know that lat has at least one element
 
-(defun insertR
-    (lambda (new old lat)
+(defun insertR (new old lat)
       (cond ((eq? old (car lat)) (cons old (cons new (cdr lat))))
 	    (t (cons (car lat)
 		     (insertR (new
 			       old
-			       (cdr lat))))))))
+			       (cdr lat)))))))
 
 
 (insertR 'e 'd '(a b c d f))  ; (a b c d e f)
