b02670cfa4ad2d1d7361e46effc7ce8f5f876833
Nicerova7 <nil_al_7@hotmail.com>
2017-03-15 23:24:52 -0500

    N-veces in lat

---
+++ 4_NumbersGames/occur.el
@@ -0,0 +1,11 @@
+;; Counts the number of times an atom a appears in a lat
+
+;; Remember add1 function
+
+(defun occur (a lat)
+  (cond ((null lat) 0)
+	(t (cond ((eq a (car lat)) (add1 (occur a (cdr lat))))
+		 (t (occur a (cdr lat)))))))
+
+
+(occur 'a '(a b a c a d)) ; 3
