172c648f2169d4242d0aa4f62c75f9b514d23b68
Nicerova7 <nil_al_7@hotmail.com>
2017-03-27 21:58:00 -0500

    Eq list simplified

--- 5_OhMyGawd_ItsFullOfStars/eqlistsmall.el
+++ 5_OhMyGawd_ItsFullOfStars/eqlistsmall.el
@@ -1,10 +1,10 @@
 ;; Eqlistsmall function is a function eqlist simplified
 
-;; Remember the equaldude function in this chapter (5)
+;; Remember the equaldude file in this chapter (5)
 
 (defun eqlistsmall(l1 l2)
   (cond ((and (null l1) (null l2)) t)
 	((or (null l1) (null l2)) nil)
-	(t (and (equaldude (car l1) (car l2))
+	(t (and (equal? (car l1) (car l2))
 		(eqlistsmall (cdr l1) (cdr l2))))))
 
