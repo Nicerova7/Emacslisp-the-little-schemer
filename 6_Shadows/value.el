62169fd59cefa9b93a35925d7b600bc5dc7a87bd
Nicerova7 <nil_al_7@hotmail.com>
2017-03-30 00:27:47 -0500

    Value function

---
+++ 6_Shadows/value.el
@@ -0,0 +1,15 @@
+;; Value function
+
+;; Remember the last functions (operator , 1st-sub-exp and 2nd-sub-exp )
+;; Remember the function eq? in equaldude file in chapter 5 and chapter 4
+;; expon file (simbolexp function)
+
+(defun value (nexp)
+  (cond ((atom aexp) nexp)
+	((eq? (operator nexp) '+)
+	 (+ (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+	((eq? (operator aexp) 'x)
+	 (x (value (1st-sub-exp nexp)) (value (2nd-sub-exp nexp))))
+	(t (simbolexp (value (1st-sub-exp nexp))
+		      (value (2nd-sub-exp aexp))))))
+
