66130ed6f87d62ff69f0625ebf1cc3ef58ab5b73
Nicerova7 <nil_al_7@hotmail.com>
2017-04-09 12:42:54 -0500

    insertR-F

---
+++ 8_LambdaTheUltimate/insertR-f.el
@@ -0,0 +1,14 @@
+;; InsertR-f
+
+(eval
+ '(defun insertR-f (test?)
+    (function
+     (lambda (new old l)
+       (cond ((null l) '())
+	     ((funcall test? (car l) old) (cons old (cons new (cdr l))))
+	     (t (cons (car l)
+		      (funcall (insertR-f test?) new old (cdr l))))))))
+ t)
+
+
+(funcall (insertR-f 'eq) 'a 'n '(a b c n)) ; (a b c n a)
