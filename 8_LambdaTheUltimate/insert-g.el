753ceb6e42d1069818379053bff031d84e7089df
Nicerova7 <nil_al_7@hotmail.com>
2017-04-09 13:05:32 -0500

    insert-g

---
+++ 8_LambdaTheUltimate/insert-g.el
@@ -0,0 +1,15 @@
+;; insert-g Function
+
+(eval
+ '(defun insert-g (seq)
+    (function
+     (lambda (new old l)
+       (cond ((null l) '())
+	     ((eq (car l) old) (funcall seq new old (cdr l)))
+	     (t (cons (car l)
+		      (funcall (insert-g seq) new old (cdr l))))))))
+ t)
+
+
+(funcall (insert-g 'seqL) 'a 'b '(a c b d f b)) ; (a c a b d f b)
+
