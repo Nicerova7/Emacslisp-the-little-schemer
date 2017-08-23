12505e6ead4cdb2cdb0d4cf44242fc329f028d62
Nicerova7 <nil_al_7@hotmail.com>
2017-08-23 12:11:34 -0500

    Evens-only*

---
+++ 8_LambdaTheUltimate/even-star.el
@@ -0,0 +1,17 @@
+;; It is even ?
+
+(defun even? (n)
+  (= (* (/ n 2) 2) n))
+
+;; 4 t 
+;; 5 nil  (notee : wrong in 5.0)
+
+(defun even-star (l)
+  (cond ((null l) '())
+	((atom (car l))
+	 (cond ((even? (car l)) (cons (car l)
+				      (even-star (cdr l))))
+	       (t (even-star (cdr l)))))
+	(t (cons (even-star (car l))
+		 (even-star (cdr l))))))
+
