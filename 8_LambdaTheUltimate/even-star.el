4f1be91be2ba515d71fa75345973d259f5398921
Nicerova7 <nil_al_7@hotmail.com>
2018-01-09 23:40:42 -0500

    even-only*

--- 8_LambdaTheUltimate/even-star.el
+++ 8_LambdaTheUltimate/even-star.el
@@ -15,3 +15,5 @@
 	(t (cons (even-star (car l))
 		 (even-star (cdr l))))))
 
+(even-star '((9 1 2 8) 3 10 ((9 9) 7 6) 2)) ; ((2 8) 10 (nil 6) 2)
+                                            ; ((2 8) 10 ('() 6) 2)
