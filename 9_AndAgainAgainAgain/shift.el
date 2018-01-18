385da26a0da1b095465a8c3658d1bd36d97b2b2e
Nicerova7 <nil_al_7@hotmail.com>
2018-01-17 21:36:17 -0500

    shift

--- 9_AndAgainAgainAgain/shift.el
+++ 9_AndAgainAgainAgain/shift.el
@@ -8,8 +8,12 @@
 (defun second (l)
   (car (cdr l)))
 
+;; you need a cons like a show here because
+;; we're going to use it to create a new list in shift function
 (defun build (a b)
-  (cons a b))
+  (cons a (cons b '())))
+
+;;In this way a list will be given two values without any problem
 
 (defun shift (pair)
   (build (first (first pair))
